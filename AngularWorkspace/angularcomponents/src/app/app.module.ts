4ecdab7866117cec026786f8965a66e0cf23bdb0
kokanevina <kokane.vina@gmail.com>
2022-06-20 12:26:28 +0530

    Components
    Navbar components

--- AngularWorkspace/angularcomponents/src/app/app.module.ts
+++ AngularWorkspace/angularcomponents/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
