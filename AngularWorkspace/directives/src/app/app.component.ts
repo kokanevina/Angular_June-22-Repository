abb4e1f2ae143627d6a2480de687f6d39d5bb26f
kokanevina <kokane.vina@gmail.com>
2022-06-22 13:18:58 +0530

    Attribute Directives
    ngStyle ngClass

--- AngularWorkspace/directives/src/app/app.component.ts
+++ AngularWorkspace/directives/src/app/app.component.ts
@@ -15,4 +15,24 @@ export class AppComponent {
   hotD="";
   perc=45;
   bgColor='yellow';
+
+ myStyle={
+  border:'5px solid pink',   /*javasciprt keys */
+  backgroundColor:this.bgColor,
+  color:'blue',
+  'margin-top':'10px' /*css key is allowed in javascript object */
+ }
+ setBg(){
+  console.log("In function");
+  this.myStyle.backgroundColor=this.bgColor;
+  this.myStyle['margin-top']='60px';  // another way to access object keys
+ }
+
+ styleArray=['fontClass', 'borderClass', 'colorClass'];
+
+ myClassStyle={
+  'fontClass':false,
+  'borderClass':true,
+  'colorClass':true
+ }
 }
