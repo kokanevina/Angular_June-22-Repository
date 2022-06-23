818a830b03b0133d2191353bff67d7414d31bb5c
kokanevina <kokane.vina@gmail.com>
2022-06-23 11:18:35 +0530

    Angular Pipes
    strings, number

---
+++ AngularWorkspace/angularpipes/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angUlaR-piPEs demO';
+  india="india-Is-MY-cOUNtry"
+
+  salary=78900.457345;
+
+  salaryArray=[78900.457345,78900,76567.12, 56455.777,76789.5, 50000.4444]
+
+}
