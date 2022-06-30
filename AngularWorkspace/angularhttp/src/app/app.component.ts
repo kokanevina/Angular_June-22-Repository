ed6431ecc7f461f9711251bbaf9628178a88078e
kokanevina <kokane.vina@gmail.com>
2022-06-30 11:11:10 +0530

    Angular http
    get

---
+++ AngularWorkspace/angularhttp/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angularhttp';
+  trains:any[]=[];
+  constructor(private _http:HttpClient){
+
+  }
+  getJsonData(){
+    this._http.get("http://localhost:4200/assets/Db.json/")
+      .subscribe({
+        next:successR=>
+          console.log(successR)  ,
+        error:errorR=>console.log(errorR)
+      }
+      );
+  }
+}
